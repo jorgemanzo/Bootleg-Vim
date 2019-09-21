e078643278f2b4fa62793306cd23397366634aa6
jorgemanzo <jmanzo203689@gmail.com>
2019-09-20 20:49:42 -0700

    Initial Commit

---
+++ Bootleg-Vim/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bootleg-Vim
+//
+//  Created by Jorge on 9/20/19.
+//  Copyright © 2019 jorgemanzo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
