4c219668e1f03ea4b9145b61980e8ed180500cce
jorgemanzo <jmanzo203689@gmail.com>
2019-09-21 16:48:58 -0700

    Editor starts in normal mode instead

--- Bootleg-Vim/EditorModeManager.swift
+++ Bootleg-Vim/EditorModeManager.swift
@@ -10,12 +10,12 @@ import Foundation
 
 /**
 	Manages the current state of the text editor.
-	Change the mode here doesnt actually disable
-	or enable the editor. The `NSTextView` instance
-	disables or enables editing. This object is
-	used to keep track of the mode and notify
-	the delegate (`ViewController`) that the mode
-	was changed.
+	Used by `TextView` to keep track of what mode it
+	is in.
+
+	On changing mode, uses a `NotifyEditorDelegate`
+	to send a message to a delegate (usually a View
+	Controller) of the change.
 */
 class EditorModeManager {
 	public static let INSERT_MODE = "-- INSERT_MODE --"
@@ -24,10 +24,14 @@ class EditorModeManager {
 	weak var delegate: NotifyEditorDelegate?
 	
 	init() {
-		currentMode = EditorModeManager.INSERT_MODE
+		// Editor should start in normal mode
+		currentMode = EditorModeManager.NORMAL_MODE
 		delegate?.onModeChange(modeWasChangedTo: currentMode)
 	}
 	
+	/**
+	* Gets the current mode that the editor is in.
+	*/
 	public func getCurrentMode() -> String {
 		return currentMode
 	}
@@ -50,6 +54,24 @@ class EditorModeManager {
 	
 }
 
+/**
+	Whenever `NotifyEditorManager` changes the current
+	mode of the editor, `onModeChange(modeWasChangedTo:)`
+	is called with the current mode the editor is in.
+*/
 protocol NotifyEditorDelegate: AnyObject {
+	/**
+		This method is called whenever the mode is changed
+		in the `EditorModeManager`.
+	
+		Implement this method to know when the editor has
+		changed its mode.
+	
+		- Parameters:
+			- modeWasChangedTo: The new mode the editor
+		was changed to. It takes the value of eiter
+		`EditorModeManager.INSERT_MODE` or `EditorModeManager.NORMAL_MODE`
+		Strings.
+	*/
 	func onModeChange(modeWasChangedTo newMode: String)
 }
--- Bootleg-Vim/TextView.swift
+++ Bootleg-Vim/TextView.swift
@@ -11,16 +11,32 @@ import Cocoa
 	Subclasses NSTextView to become its own NSTextViewDelegate
 	and get notified of information from the text view.
 */
-class TextView: NSTextView, NSTextViewDelegate {
-	
+class TextView: NSTextView {
+
 	/// Represents the current mode of the text editor
 	public let editorModeManager = EditorModeManager()
+
+	
+	// MARK: How we detect escape key pressed
+	override func cancelOperation(_ sender: Any?) {
+		
+		// Since escape was pressed, change to normal mode
+		editorModeManager.changeMode(changeModeTo: EditorModeManager.NORMAL_MODE)
+	}
+	
+
+}
+
+/**
+* Moved methods that are conforming to protocol
+* into extension
+*/
+extension TextView: NSTextViewDelegate {
 	
 	// MARK: How we detect regular keys and numbers
 	func textView(_ textView: NSTextView, shouldChangeTextIn affectedCharRange: NSRange, replacementString: String?) -> Bool {
 		let currentMode: String = editorModeManager.getCurrentMode()
 		let pressedKey: String = replacementString ?? ""
-		print(pressedKey)
 		if currentMode == EditorModeManager.INSERT_MODE {
 			return true
 		}
@@ -28,14 +44,19 @@ class TextView: NSTextView, NSTextViewDelegate {
 			if pressedKey == "i" {
 				editorModeManager.changeMode(changeModeTo: EditorModeManager.INSERT_MODE)
 			}
+			else if pressedKey == "h" {
+				moveLeft(nil)
+			}
+			else if pressedKey == "j" {
+				moveDown(nil)
+			}
+			else if pressedKey == "k" {
+				moveUp(nil)
+			}
+			else if pressedKey == "l" {
+				moveRight(nil)
+			}
 		}
 		return false
-	}
-	
-	// MARK: How we detect escape key pressed
-	override func cancelOperation(_ sender: Any?) {
-		
-		// Since escape was pressed, change to normal mode
-		editorModeManager.changeMode(changeModeTo: EditorModeManager.NORMAL_MODE)
 	}
 }
